7e6ed1b2467ad3c2a4c64c6d470351c18f164cc6
Your Name git config --global user.name Your <LJQ1174880845@163.com>
2019-01-24 16:19:39 +0800

    修改冲突

--- maoyandiany/src/components/Xlongdong.vue
+++ maoyandiany/src/components/Xlongdong.vue
@@ -8,6 +8,20 @@
     export default {
        created(){
             console.log(this.$route.params.id)
+
+            //请求数据
+            let xhr = new XMLHttpRequest();
+                xhr.onload = ()=>{
+                    if(xhr.status===200){
+                        let res = JSON.parse(xhr.responseText);
+                        console.log(res)
+                        res=res.cinemas
+                        this.movieList=res
+                    }
+                }
+            xhr.open('get','http://localhost:4008/movie/ajax/cinemaList?day=2019-01-22&offset=0&limit=20&districtId=-1&lineId=-1&hallType=-1&brandId=-1&serviceId=-1&areaId=-1&stationId=-1&item=&updateShowDay=true&reqId=1548155918452&cityId=20',true);
+            xhr.send();
+
        }
     }
     
